4898a83e7274427e7479582dd2de9b9d94544803
Rob Rosenbaum <rob@robrosenbaum.com>
2013-09-07 09:29:14 -0700

    not working yet...

--- block_resolver.js
+++ block_resolver.js
@@ -1,4 +1,5 @@
 var H = require('./helpers.js');
+var last = H.last; // I use this too often to write it out all the time
 
 var BLOCK_TAGS = [
 	'FOR', 'WHILE', 'DO', 'IF', 'ELSE',
@@ -17,10 +18,11 @@ function trimNewlines(tokens, i) {
 	return trimmed;
 }
 
-function UnindentedFunc(level, indents, pairs) {
+function Line(pos, level, indented, has_block) {
+	this.pos = pos;
 	this.level = level;
-	this.indents = indents;
-	this.pairs = pairs;
+	this.indented = indented;
+	this.has_block = has_block;
 }
 
 function Resolver(tokens) {
@@ -31,26 +33,41 @@ function Resolver(tokens) {
 	this.level = '';
 	this.awaiting_block = false;
 	this.is_do = false;
+	this.blocks = [];
+
 	this.pairs = 0;
 	this.indents = 0;
 
-	this.lines = [{ pos: this.pos, level: this.level, indented: false }];
-	this.unindented_funcs = [];
+	this.indents_at_level = { '': 0 };
+
+	this.lines = [new Line(this.pos, this.level, false, false)];
 }
 
 Resolver.prototype.indent = function() {
-	var line = H.last(this.lines);
+	var line = last(this.lines);
 
-	line.indented = true;
 
 	if (this.awaiting_block) {
+		line.has_block = true;
 		this.awaiting_block = false;
 		this.level = line.level + this.tokens[this.pos][1];
+
+		// only update this if our INDENT was not added
+		// i.e., if it actually changes the indentation
+		if (this.level !== line.level) {
+			this.indents_at_level[this.level] = this.indents;
+		}
+
+		if (this.tokens[this.pos][1]) {
+			var new_line = new Line(this.pos, this.level, false, false);
+			this.lines.push(new_line);
+		}
+
+		this.blocks.push(H.last(this.lines));
 		this.indents++;
 		this.pos++;
-
-		this.lines.push({ pos: this.pos, indented: false, level: this.level });
 	} else {
+		line.indented = true;
 		// erase indent
 		this.removeToken(this.pos);
 		this.pos--;
@@ -58,20 +75,22 @@ Resolver.prototype.indent = function() {
 };
 
 Resolver.prototype.outdent = function() {
-	var line = H.last(this.lines);
-
+	var line = last(this.lines);
 	var m_indent = H.clipString(this.level, this.tokens[this.pos][1]);
+	var ln;
 
 	if (m_indent.isNothing()) {
 		H.error('Mismatched indent', this.tokens[this.pos]);
 	} else {
 		this.level = m_indent.fromJust();
 
+		console.log(this.lines, [line.level, this.level]);
+
 		if (line.indented && line.level === this.level) {
 			line.indented = false; // we've undone the indentation now
 
 			// replace OUTDENT with TERMINATOR, and try again
-			if ('TERMINATOR' === this.tokens[this.pos + 1][0]) {
+			if (this.nextTagIs('TERMINATOR')) {
 				this.removeToken(this.pos);
 			} else {
 				this.replaceTag(this.pos, 'TERMINATOR');
@@ -79,15 +98,25 @@ Resolver.prototype.outdent = function() {
 				
 			this.pos--;
 		} else {
+			this.blocks.pop();
+			this.lines.pop();
 			this.indents--;
-			if (this.closingUnindentedFunction()) {
-				this.unindented_funcs.pop();
+
+			ln = last(this.lines);
+
+			// close innermost unclosed indent
+			// outer ones will be closed at the next outdent (which we are inserting now)
+			if (this.indents > this.indents_at_level[this.level]) {
+				this.insertTag(this.pos + 1, 'OUTDENT');
 			}
 
-			this.lines.pop();
-
-			if (line.level === this.level) {
-				this.nextLine();
+			if (ln.level === this.level) {
+				// if line is indented but not outdented, fix that
+				if (ln.indented) {
+					this.insertTag(this.pos + 1, 'OUTDENT');
+				} else {
+					this.nextLine();
+				}
 			}
 		}
 	}
@@ -98,10 +127,7 @@ Resolver.prototype.functionBlock = function() {
 	this.awaiting_block = true;
 
 	// take care of inline functions
-	if ('INDENT' !== this.tokens[this.pos + 1][0] ) {
-		this.unindented_funcs.push(
-			new UnindentedFunc(this.level, this.indents, this.pairs)
-		);
+	if (! this.nextTagIs('INDENT')) {
 		this.insertTag(this.pos + 1, 'INDENT');
 	}
 };
@@ -110,12 +136,12 @@ Resolver.prototype.functionBlock = function() {
 // prior to the line, but after accounting for the new indent level
 Resolver.prototype.nextLine = function() {
 	this.lines.push(
-		{ pos: this.pos + 1, indented: false, level: this.level }
+		new Line(this.pos + 1, this.level, false, false)
 	);
 };
 
 Resolver.prototype.terminator = function() {
-	if (H.last(this.lines).indented) {
+	if (last(this.lines).indented) {
 		// erase newline
 		this.removeToken(this.pos);
 		this.pos--;
@@ -126,8 +152,9 @@ Resolver.prototype.terminator = function() {
 };
 
 Resolver.prototype.closingUnindentedFunction = function() {
-	var innermost = H.last(this.unindented_funcs);
-	return innermost.pairs === this.pairs &&
+	var innermost = last(this.unindented_funcs);
+	return last(this.lines) === innermost.line &&
+		// innermost.pairs === this.pairs &&
 		innermost.indents === this.indents;
 }
 
@@ -174,6 +201,11 @@ Resolver.prototype.fixBlocks = function() {
 	}
 }
 
+Resolver.prototype.nextTagIs = function(tag) {
+	return this.tokens[this.pos + 1] &&
+		tag === this.tokens[this.pos + 1][0];
+}
+
 Resolver.prototype.removeToken = function(pos) {
 	return this.tokens.splice(pos, 1);
 }
@@ -190,7 +222,7 @@ Resolver.prototype.replaceTag = function(pos, tag) {
 }
 
 Resolver.prototype.debug_tokens = function(i, indent_level, awaiting_block) {
-	var line = H.last(this.lines);
+	var line = last(this.lines);
 
 	for (var j = 0; j < this.tokens.length; j++) {
 		var tok = [this.tokens[j][0], this.tokens[j][1]];
@@ -211,7 +243,7 @@ function resolveBlocks(tokens) {
 
 	r.fixBlocks();
 
-	if (H.last(tokens)[0] !== 'TERMINATOR') {
+	if (last(tokens)[0] !== 'TERMINATOR') {
 		r.insertTag(tokens.length, 'TERMINATOR');
 	}
 
