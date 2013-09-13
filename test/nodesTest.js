e7d51912a0b70546a744ddfb6cdf88dcb2a2385d
Rob Rosenbaum <rob@robrosenbaum.com>
2013-09-12 19:57:21 -0700

    Added test for nodes.ls

---
+++ test/nodesTest.js
@@ -0,0 +1,33 @@
+var H = require('../helpers.js');
+var N = require('../nodes.js');
+
+function doesntThrow(assert, f, err) {
+	try {
+		f();
+	} catch (e) {
+		assert.doesNotThrow(function () {
+			throw e;
+		}, err, e.toString());
+	}
+}
+
+function map(xs, f) {
+	var ys = {};
+	for (var i in xs) { if (xs.hasOwnProperty(i)) {
+		ys[i] = f(xs[i]);
+	}}
+	return ys;
+}
+
+module.exports = {
+	'Variables defined in Code do not show up in outer scope': function(b, assert) {
+		var
+			x_equals_3 = new N.Assign(new N.Identifier('x'), '=', new N.Literal('3')),
+			func = new N.Code([], new N.Block([x_equals_3])),
+			b = new N.Block([func]);
+
+		// Code uses vars_defined(this.block)
+		assert.eql(['x'], N.vars_defined(func.block));
+		assert.eql([], N.vars_defined(b));
+	}
+};
